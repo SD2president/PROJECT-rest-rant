23d86bc659747f72ca83fd28692b5dc86cd83e18
Shigeyuki Isorena <Isorenashigeyuki21@gmail.com>
2022-07-06 17:40:19 -0700

    added 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//the .env file summon
+require('dotenv').config
+
+//the express package 
 const express = require('express')
 const app = express()
 
@@ -5,4 +9,9 @@ app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
-app.listen(1337)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404</h1>')
+})
+
+
+app.listen(process.env.PORT)
