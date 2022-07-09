53ca3e3b1560997c1747f17de62aee18c8d661f5
Shigeyuki Isorena <Isorenashigeyuki21@gmail.com>
2022-07-09 11:38:55 -0700

    adding 404 page view

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404</h1>')
+    res.render('error404')
 })
 
 
